fd90bb04218f2f0ee6fbf5788480d45c9ff1fa87
polmonta <polmonta05@gmail.com>
2026-05-21 20:32:48 +0200

    Replaced wrong keys

--- lib/users.ts
+++ lib/users.ts
@@ -4,10 +4,10 @@ export interface TeamMember {
 }
 
 export const TEAM_MEMBERS: TeamMember[] = [
-  { name: "Pol Giralt", apiKey: "bYsoVgQcRU3UZA9C2wXoz1hskWEfFuR99qYHdWAX" },
+  { name: "Pol Giralt", apiKey: "OKHQ2hA6U54WVAumy3nwgQ08jAvAKkLTYeEiwY08" },
   { name: "Pol Montanera", apiKey: "ugPOGkjUYCWqvg44IVctHD1IukghFOxTCqdssbrZ" },
   { name: "Noel Freire", apiKey: "kmOASxuhCU5P7EBd8kptg8PnsAzotaVhyXcd8foI" },
-  { name: "Sergi Galán", apiKey: "3K9jyHSH7bevzQVRPIkh7iQBsyjdCsu8zCudsnsM" },
+  { name: "Sergi Galán", apiKey: "c7vPxtIJ7bh3N4pAt9rMTRA20s5rH830unTNJItb" },
   { name: "Fernando Joel Alcívar", apiKey: "Igydyc0Qize4YEcKb9hH1IlWaDtPNhBSxGyLMurB" },
   { name: "Unauthenticated User", apiKey: "" }
 ];
